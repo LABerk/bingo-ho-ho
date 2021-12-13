335a718e4b49db0a61d2b2d1e7bb9e7d8ce09b8e
Laura Berkey <berkeylaura@gmail.com>
2021-12-13 13:20:07 -0600

    onClick works

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
 
 function Board() {
   return (
-    <div className="Board">
-      <p>Board</p>
-    </div>
+    <Table>
+      <tbody>
+        <tr>
+          <td
+            onClick={() => {
+              console.log("hi");
+            }}
+          >
+            1
+          </td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>6</td>
+          <td>7</td>
+          <td>8</td>
+        </tr>
+        <tr>
+          <td>9</td>
+          <td>10</td>
+          <td>11</td>
+          <td>12</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
